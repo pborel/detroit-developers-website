fc6afac852741149e94239813476ce6e3ea3ee35
Phil <phil@rivet.work>
2026-03-22 20:13:41 -0400

    fix past events date logic

--- eleventy.config.js
+++ eleventy.config.js
@@ -38,7 +38,9 @@ export default function (eleventyConfig) {
   });
 
   eleventyConfig.addFilter("isUpcoming", (date) => {
-    return new Date(date) >= new Date();
+    const d = new Date(date);
+    const dayAfter = new Date(Date.UTC(d.getUTCFullYear(), d.getUTCMonth(), d.getUTCDate() + 1));
+    return dayAfter > new Date();
   });
 
   eleventyConfig.addFilter("timeFormat", (date) => {
